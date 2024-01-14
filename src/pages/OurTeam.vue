c24d32347b8a889815fa8ff3b0e836fe30566d5b
nulviet <nulvietz@gmail.com>
2024-01-14 20:17:53 +0100

    first commit

---
+++ src/pages/OurTeam.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page padding>
+    <div class="cards row justify-between">
+      <q-card class="col-4">
+        <q-card-section class="text-center">
+          <p>Hrystia Pavlutska</p>
+        </q-card-section>
+        <q-card-section class="text-center">
+          <p>Photo</p>
+        </q-card-section>
+        <q-card-section class="text-center">
+          <p>Description</p>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="col-4">
+        <q-card-section class="text-center">
+          <p>Ola Sheredko</p>
+        </q-card-section>
+        <q-card-section class="text-center">
+          <p>Photo</p>
+        </q-card-section>
+        <q-card-section class="text-center">
+          <p>Description</p>
+        </q-card-section>
+      </q-card>
+
+      <q-card class="col-4">
+        <q-card-section class="text-center">
+          <p>Vira Pavlutska</p>
+        </q-card-section>
+        <q-card-section class="text-center">
+          <p>Photo</p>
+        </q-card-section>
+        <q-card-section class="text-center">
+          <p>Description</p>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+}
+</script>
---
+++ src/pages/WhoAreWe.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-page>
+    <q-parallax
+
+      src="../assets/loadPage_main.jpg"
+      :height="1000"
+      :speed="1"
+    >
+      <h1 class="text-white">
+        Windmill Tree
+      </h1>
+    </q-parallax>
+    <div
+      class="text-center row justify-center contacts"
+    >
+      <i
+        class="fa-regular fa-envelope fa-4x cursor-pointer"
+        style="padding: 10px 45px"
+      />
+
+      <i
+        class="fa-brands fa-facebook fa-4x cursor-pointer"
+        style="padding: 10px 45px"
+      />
+
+      <i
+        class="fa-brands fa-instagram fa-4x cursor-pointer"
+        style="padding: 10px 45px"
+      />
+
+      <i
+        class="fa-brands fa-youtube fa-4x cursor-pointer"
+        style="padding: 10px 45px"
+      />
+
+      <i
+        class="fa-brands fa-whatsapp fa-4x cursor-pointer"
+        style="padding: 10px 45px"
+      />
+    </div>
+
+    <!-- <q-img
+      src="../assets/loadPage_main.jpg"
+      style=""
+    /> -->
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'WhoAreWe'
+}
+</script>
